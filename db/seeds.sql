b0ae500ee19bc9f42d12d6c9d55c7f658ed9c440
rachela1 <rachel_kaitlin01@hotmail.com>
2023-07-17 18:01:45 -0500

    added seeds and schema files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE employee_db;
+
+INSERT INTO department (name) 
+    VALUES ('Plumbing'), ('Electrical');
+
+INSERT INTO role (title, salary) 
+    VALUES ('Manager, 50000'), ('Supervisor, 30000');
+
+ INSERT INTO employee (first_name, last_name) 
+    VALUES ('Bob', 'Orange'), ('John', 'Apple');
