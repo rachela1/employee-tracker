6db8abdd51aa4cf27eb18526b3b202b5adbead62
rachela1 <rachel_kaitlin01@hotmail.com>
2023-07-18 12:22:46 -0500

    added prompt text

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const consTab = require('console.table');
 
 const prompt = inquirer.createPromptModule();
 const db = mysql.createConnection({
@@ -8,3 +9,21 @@ const db = mysql.createConnection({
     password: '',
     database: 'employee_db',
 }, console.log('Connected'));
+
+const init = () => {
+    inquirer
+        .prompt({
+            type: 'list', 
+            message: 'Select one from the following options:',
+            name: 'initialize',
+            choices: [
+                'View departments',
+                'View roles',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update employee role',
+                'All done'
+            ]
+        })
+}
